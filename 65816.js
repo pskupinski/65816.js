ae14e7382af4c82352ee56cdaa0a52df3d5fa724
Preston Skupinski <preston@liatriks.com>
2012-12-02 23:18:41 -0500

    Update the TODO and the copyright email since msu only provides you with access to your @msu.edu email address for two years after graduation

--- 65816.js
+++ 65816.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2012, Preston Skupinski <skupinsk@cse.msu.edu>
+ * Copyright (c) 2011-2012, Preston Skupinski <preston.skupinski@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
